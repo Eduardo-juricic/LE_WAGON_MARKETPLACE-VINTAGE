36c05b621bdf377e326e6ab6cb3a5feaaafce309
Eduardo Juricic <edujuricic@gmail.com>
2025-03-07 14:38:10 -0300

    arrumando os controllers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,4 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_order, only: [:show, :update, :destroy]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :orders, dependent: :destroy
   has_many :products, dependent: :destroy
 
-  validates :name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
