88c80ec1c8557617beda6634698e244bc69cfef4
Eduardo Juricic <edujuricic@gmail.com>
2025-03-07 18:01:39 -0300

    View dos produtos feita

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,4 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
-
   def index
     @products = Product.all
   end
