1d8413ae02eaf8625ce775ec787ab9d0dc4d7102
Amanda Miquelino <amanda.miquelino2@gmail.com>
2025-03-10 16:17:26 +0100

    criei o método e a view de orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,17 @@
 class OrdersController < ApplicationController
+  def create
+    @order = Order.new(product_id: params[:product_id])
+    @order.user = current_user
+    @order.status = "pending"
+
+    if @order.save
+      redirect_to orders_path, notice: 'Pedido criado com sucesso!' 
+    else
+      redirect_to root_path, alert: 'Erro ao criar o pedido.'
+    end
+  end
+
+  def index
+    @orders = current_user.orders
+  end
 end
