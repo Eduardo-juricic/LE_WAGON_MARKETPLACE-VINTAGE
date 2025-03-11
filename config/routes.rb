eb929e615e1b1442f595d5a50059873f491d4149
Eduardo Juricic <edujuricic@gmail.com>
2025-03-11 17:24:08 -0300

    fiz o botao deletar dos meus pedidos

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :products
-  resources :orders, only: [:index, :create, :show]
+  resources :orders
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
