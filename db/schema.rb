b5986391bd464f7d0ac4773d8bf7ad6414a874d5
Amanda Arruda <amandaolia@hotmail.com>
2025-03-12 15:56:08 +0000

    atualizei orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_10_142716) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_12_151010) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_10_142716) do
     t.bigint "user_id", null: false
     t.bigint "product_id", null: false
     t.string "status", default: "pendente"
+    t.integer "quantity"
     t.index ["product_id"], name: "index_orders_on_product_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
